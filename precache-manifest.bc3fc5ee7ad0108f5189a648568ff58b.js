f537b215bc270e1453b13fa17b6918834baa7bf2
santosbaterias <santos.baterias.ti@gmail.com>
2021-04-16 23:34:30 -0300

    Updates

---
+++ precache-manifest.bc3fc5ee7ad0108f5189a648568ff58b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374cf100adb7084b48468cde8e5645de",
+    "url": "/santos-baterias/index.html"
+  },
+  {
+    "revision": "9adcc37e0f2ce38d0c85",
+    "url": "/santos-baterias/static/css/2.468338fd.chunk.css"
+  },
+  {
+    "revision": "c40732f50666a7f28dc5",
+    "url": "/santos-baterias/static/css/main.d814c64a.chunk.css"
+  },
+  {
+    "revision": "9adcc37e0f2ce38d0c85",
+    "url": "/santos-baterias/static/js/2.1777fe1d.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/santos-baterias/static/js/2.1777fe1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40732f50666a7f28dc5",
+    "url": "/santos-baterias/static/js/main.af4a72c3.chunk.js"
+  },
+  {
+    "revision": "ff59b4a494b05f3137dc",
+    "url": "/santos-baterias/static/js/runtime-main.b159ae27.js"
+  },
+  {
+    "revision": "a1ba2c0aef73aec74f54da83270ce4aa",
+    "url": "/santos-baterias/static/media/Logo.a1ba2c0a.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/santos-baterias/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/santos-baterias/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/santos-baterias/static/media/revicons.2feb69cc.eot"
+  }
+]);
